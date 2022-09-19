014abbf7c27966b747058e8bdaf593d98e5c1bbe
Facundo Aquino <facun90@live.com>
2022-09-19 10:19:03 -0300

    Generics

--- src/Generics/GenericsJava.java
+++ src/Generics/GenericsJava.java
@@ -1,4 +1,27 @@
 package Generics;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class GenericsJava {
+    //introduced jdk 5
+    public static void main(String[] args) {
+        List<String> clientes = new ArrayList();
+        clientes.add("some string");
+        clientes.add("some string 1");
+        clientes.add("some string 2");
+        System.out.println("clientes = " + clientes);
+        System.out.println("clientes = " + clientes.get(2));
+    }
+
+    public static <T> List<T> fromArrayToList(T[] c){
+        return Arrays.asList(c);
+    }
+
+//    podemos tener mas de un parametro generico
+    public static <T, G> List<T> fromArrayToList(T[] c, G g ){
+        System.out.println("g = " + g);
+        return Arrays.asList(c);
+    }
 }
