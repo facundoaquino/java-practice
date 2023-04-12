bee0f5163553722185133aa2c5e28d54d09d6385
facundo aquino <facun90@live.com>
2023-04-12 09:17:18 -0300

    Optinal class

--- src/OptinalClass.java
+++ src/OptinalClass.java
@@ -1,3 +1,4 @@
+import java.util.Locale;
 import java.util.Optional;
 
 public class OptinalClass {
@@ -25,11 +26,64 @@ public class OptinalClass {
 
         //        optinal con un valor  nulo y supplier
         Optional<String> pepe3 = Optional.ofNullable(null);
-        String siPepe2 = pepe3.orElseGet( () -> {
+        String siPepe2 = pepe3.orElseGet(() -> {
             String value = "plus";
             return "something" + value;
         });
         System.out.println(siPepe2);
-        
+
+//          valor nulo y orElseThrow
+
+        Optional<String> optStr_1 = Optional.ofNullable("str");
+        String str_1 = optStr_1
+                .map(String::toUpperCase)
+                .orElseThrow(IllegalStateException::new);
+
+        //          ifPresent
+
+        Optional<String> optStr_2 = Optional.ofNullable("str");
+        optStr_2
+                .ifPresent(s1 -> {
+                    System.out.println(s1);
+                });
+
+        //          ifPresentOrElse recibe dos parametros
+
+        Optional<String> optStr_3 = Optional.ofNullable("str");
+        optStr_3
+                .ifPresentOrElse(s1 -> {
+                    System.out.println(s1);
+                },() -> {
+                    System.out.println("or else option");
+                });
+
+
+//
+
+        Person person = new Person("james", null);
+        String email = person
+                .getEmail()
+                .map(String::toLowerCase)
+                .orElse("not email provided");
+        System.out.println(email);
+    }
+}
+
+class Person {
+    private final String name;
+    private final String email;
+
+    public String getName() {
+        return name;
+    }
+
+//    podemos asumir que el email puede ser null
+    public Optional<String> getEmail() {
+        return Optional.ofNullable(email);
+    }
+
+    public Person(String name, String email) {
+        this.name = name;
+        this.email = email;
     }
 }
